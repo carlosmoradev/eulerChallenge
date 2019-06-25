c27bdd57146a99bf3a9da198221253ed33cf3244
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2019-06-25 15:11:04 -0500

    desafio1 resuelto

--- euler1.py
+++ euler1.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# Desafio1.  Encontrar la sumatoria de todos los multiplos de 3 y de 5 
+# inferiores a 1000
 
 limite = int(input("Indique el limite para realizar el calculo: "))
 secuencia = 0
@@ -11,12 +13,10 @@ while secuencia < limite - 1:
     print("la secuencia va en: ", secuencia)
     previo = multiplo
     print("el multiplo previo fue: ", previo)
-    # if previo != multiplo:
-    suma = multiplo + previo
-
+ 
     if secuencia%3 == 0 or secuencia%5 == 0:
         multiplo = secuencia
-        # multiplo = multiplo + valor
+        suma = suma + multiplo
         print("el multiplo es", multiplo)
     
 print("el resultado es", suma)
